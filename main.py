19e62b77b716686b99a18f2e9d271290337a7016
Nessumsar <lukasrasmusseen@gmail.com>
2024-12-10 13:46:09 +0100

    read and respond to messages

---
+++ main.py
@@ -0,0 +1,3 @@
+import travbot
+if __name__ =='__main__':
+    travbot.runBot()
---
+++ travbot.py
@@ -0,0 +1,48 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = 'x'
+
+intents=discord.Intents.default()
+intents.message_content = True
+
+
+def runBot():
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        for guild in client.guilds:
+            print(
+                f'{client.user} is connected to the following guild:\n'
+                f'{guild.name}(id: {guild.id})'
+            )
+
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        await processMessage(message)
+
+    client.run(TOKEN)    
+    
+
+def handle_user_messages(message) ->str:
+    content = message.content.lower()
+    print(message)
+    if(content == '/status'):
+        return 'Showing status for x'
+    if(content == '/update'):
+        return 'Updating x'
+    else:
+        return 'Unknown command'
+
+async def processMessage(message):
+    try:
+        botfeedback = handle_user_messages(message)
+        await message.channel.send(botfeedback)
+    except Exception as error:
+        print(error)
