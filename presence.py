fdf35661f49c18be9896a1fd8cb4eea63e6f373a
Nessumsar <lukasrasmusseen@gmail.com>
2024-12-12 14:50:49 +0100

    Presence analysis as separate file

---
+++ presence.py
@@ -0,0 +1,34 @@
+import discord
+from datetime import datetime
+import asyncio
+import pytz
+
+local_tz = pytz.timezone('Europe/Stockholm')
+woken_up_users = set()
+
+async def welcome_member_waking_up(before, after, client):
+    channel = client.get_channel(1315675558163648562)
+    for member in channel.members:
+        if after == member:
+            if before.status != after.status and after.status == discord.Status.online:
+                current_time = datetime.now(local_tz)
+                if (4 <= current_time.hour < 16) and (after.id not in woken_up_users):
+                    # Send a message in the specific channel
+                    print(f'{member} woke up at {current_time}')
+                    await channel.send(f"Good morning <@{after.id}>!")
+                    woken_up_users.add(after.id)
+
+
+async def reset_woken_up_users():
+    # Reset the woken_up_users set if the time is between 16:00 PM and 04:00 AM
+    while True:
+        now = datetime.now(local_tz)
+        print(f"Checking if the list should be reset: {now}")
+
+        # Check if it's after 16:00 PM and before 04:00 AM
+        if now.hour >= 16 or now.hour < 4:
+            print("Resetting the list of users who woke up today.")
+            woken_up_users.clear()
+
+        # Sleep for an hour and check again
+        await asyncio.sleep(3600)
--- travbot.py
+++ travbot.py
@@ -1,9 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
-from datetime import datetime
-import pytz
-import asyncio
+import presence
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,9 +11,6 @@ intents.message_content = True
 intents.members = True
 intents.presences = True
 
-woken_up_users = set()
-
-local_tz = pytz.timezone('Europe/Stockholm')
 client = None
 
 def runBot():
@@ -29,7 +24,7 @@ def runBot():
                 f'{client.user} is connected to the following guild:\n'
                 f'{guild.name}(id: {guild.id})'
             )
-        client.loop.create_task(reset_woken_up_users())    
+        client.loop.create_task(presence.reset_woken_up_users())
 
 
     @client.event
@@ -40,13 +35,7 @@ def runBot():
 
     @client.event
     async def on_presence_update(before, after):
-        if before.status != after.status and after.status == discord.Status.online:
-            current_time = datetime.now(local_tz)
-            if (4 <= current_time.hour < 16) and (after.id not in woken_up_users):
-                # Send a message in the specific channel
-                channel = client.get_channel(1315675558163648562)
-                await channel.send(f"Good morning <@{after.id}>!")
-                woken_up_users.add(after.id)
+        await presence.welcome_member_waking_up(before, after, client)
 
     client.run(TOKEN)
 
@@ -62,7 +51,7 @@ async def processMessage(message):
 def handle_user_messages(message) ->str:
     content = message.content.lower()
     for mentioned_user in message.mentions:
-        if mentioned_user.id not in woken_up_users:
+        if mentioned_user.id not in presence.woken_up_users:
             return f"{mentioned_user.name}, haven't woken up yet!"
     
     if(content.startswith("/")):
@@ -74,18 +63,3 @@ def handle_user_messages(message) ->str:
 def handle_commands(message):
     if (message == "/status"):
         return "Status x work in progress"
-
-
-async def reset_woken_up_users():
-    #Reset the woken_up_users set if the time is between 12:00 PM and 04:00 AM
-    while True:
-        now = datetime.now(local_tz)
-        print(f"Checking if the list should be reset: {now}")
-        
-        # Check if it's after 12:00 PM and before 04:00 AM
-        if now.hour >= 12 or now.hour < 4:
-            print("Resetting the list of users who woke up today.")
-            woken_up_users.clear()
-            
-        # Sleep for an hour and check again
-        await asyncio.sleep(3600)
